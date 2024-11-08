1e6af96f0302e4de4ae27c9340f9eaa301de626d
Al Fiansyah <gusjabungaf@gmail.com>
2024-11-08 08:22:48 +0700

    feat: update axios instace

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -1,2 +1,5 @@
 import axios from "axios";
-export const http = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+
+export const http = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
