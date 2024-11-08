85d6db3e0b016b16cc74c80316c778ce61534a17
Al Fiansyah <gusjabungaf@gmail.com>
2024-11-08 08:20:53 +0700

    feat: update home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,13 @@ export default async function Home() {
   const link = session ? "/dashboard" : "/login";
 
   return (
-    <div className="h-screen flex-col w-full dark:bg-black bg-white  dark:bg-grid-white/5 bg-grid-black relative flex items-center justify-center">
+    <div className="h-screen flex-col w-full dark:bg-black bg-white  dark:bg-grid-white/5 bg-grid-black/5 relative flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <h1 className="text-2xl md:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b text-center from-neutral-200 to-neutral-500 pb-4">
+      <h1 className="text-2xl md:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b text-center dark:from-neutral-200 from-neutral-500 dark:to-neutral-500 to-neutral-700 pb-4">
         Ladeva Dashboard CMS
       </h1>
-      <p className="text-xs md:text-sm max-w-5xl mx-auto text-center font-normal relative z-20 bg-clip-text text-transparent bg-neutral-400">
+      <p className="text-xs md:text-sm max-w-5xl mx-auto text-center font-normal relative z-20 bg-clip-text text-transparent dark:bg-neutral-400 bg-neutral-600">
         Ladeva Dashboard CMS adalah sistem manajemen konten yang dibuat untuk
         memudahkan pengelolaan konten website.
       </p>
