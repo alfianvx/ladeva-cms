3795ef1573919ef155cd18f96d6ab44fc6477476
alfianvx <105573370+alfianvx@users.noreply.github.com>
2024-12-16 08:55:19 +0700

    Update faq.validation.ts

--- src/types/validation/faq.validation.ts
+++ src/types/validation/faq.validation.ts
@@ -3,10 +3,8 @@ import { z } from "zod";
 export const FaqFormSchema = z.object({
   question: z
     .string()
-    .min(2, { message: "Pertanyaan tidak boleh kosong, isi minimal 2 huruf" })
-    .max(50),
+    .min(2, { message: "Pertanyaan tidak boleh kosong" }),
   answer: z
     .string()
-    .min(2, { message: "Jawaban tidak boleh kosong, isi minimal 2 huruf" })
-    .max(50),
+    .min(2, { message: "Jawaban tidak boleh kosong" }),
 });
