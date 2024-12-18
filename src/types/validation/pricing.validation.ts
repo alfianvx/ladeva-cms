1b50fff64c3c448478e33ecb2478a5481e005f46
alfianvx <gusjabungaf@gmail.com>
2024-12-18 13:37:15 +0700

    feat: set default value for is_featured field in pricing and testimonial validation schemas

--- src/types/validation/pricing.validation.ts
+++ src/types/validation/pricing.validation.ts
@@ -4,5 +4,5 @@ export const PricingFormSchema = z.object({
   title: z.string({ message: "Nama tidak boleh kosong" }),
   description: z.string({ message: "Deskripsi tidak boleh kosong" }),
   offer: z.array(z.string({ message: "Offer tidak boleh kosong" })),
-  is_featured: z.boolean().optional(),
+  is_featured: z.boolean().optional().default(false),
 });
--- src/types/validation/testimonial.validation.ts
+++ src/types/validation/testimonial.validation.ts
@@ -4,6 +4,6 @@ export const TestimonialFormSchema = z.object({
   name: z.string({ message: "Nama tidak boleh kosong" }),
   profession: z.string({ message: "Profesi / Jabatan tidak boleh kosong" }),
   message: z.string({ message: "Pesan tidak boleh kosong" }),
-  is_featured: z.boolean().optional(),
+  is_featured: z.boolean().optional().default(false),
   avatar_url: z.string().optional(),
 });
